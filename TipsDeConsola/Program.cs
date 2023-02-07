054b54ea83652f9e1ef2f4257a7a547c26940313
jizaguirre <jizaguirre@HCI-NOTE227>
2023-02-07 12:09:05 -0300

    Agregar archivos de proyecto.

---
+++ TipsDeConsola/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TipsDeConsola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Texto normal");
+            impresiones(5);
+            Console.ReadKey();
+        }
+
+        public static void impresiones(int n)
+        {
+            int x = 0;
+            int y = 0;
+
+            for (x = 0; x < n; x++)
+            {
+                for (y = 0; y < 100; y++)
+                    Console.Write(y % 10);
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
